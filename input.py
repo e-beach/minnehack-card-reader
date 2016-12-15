f7950fc9e487bce99fdb76f85c978bb5a2f3dcfc
Elliott Beach <elliott2.71828@gmail.com>
2016-12-15 16:43:54 -0600

    add code

---
+++ input.py
@@ -0,0 +1,55 @@
+import fileinput
+import re
+import spreadsheet_functions
+
+"""
+We need to
+1. read first & last name from scanned card.
+2. Locate row in google sheet that contains this name, ignoring case in format `<first-name>, <last-name>`.
+3. Update column L of the spreadsheet to record that the user is signed in.
+"""
+
+SHEET_URL = 'https://docs.google.com/spreadsheets/d/1kVZF0zbstPRkDDexa2D03Sibeg1HEtdb5fBjV_5Cw98/edit?usp=sharing'
+
+FIRST_AND_LAST_NAME = re.compile(r'[A-Z ,\.\'-]+')
+
+print spreadsheet_functions.get_names()
+names = [string.lower() for string in spreadsheet_functions.get_names()]
+
+def register_name(name):
+        "take a name and set that person as registered in the sheet"
+
+def index_of(list, item):
+    try:
+        return list.index(item)
+    except ValueError:
+        return -1
+
+def process(line):
+        matches = FIRST_AND_LAST_NAME.findall(line)
+        # Grab the last group. There may be useless matches before the end.
+        name = matches[-1]
+        # drop final initial, which is for middle name
+        first_and_last = name.split()[0] + " " + name.split()[1]
+        # Convert to lower case.
+        formatted_name = first_and_last.lower()
+        index = index_of(names, formatted_name)
+        if index == -1:
+            print 'Name %s is not registered.' % name
+        else:
+            spreadsheet_functions.check_attendance(index)
+            print 'Name %s is registered.' % name
+        return name
+
+def test():
+        data = r'%123478912347^892^^^2357892351^BEACH, ELLIOTT T?'
+        assert process(data) == 'BEACH, ELLIOTT T'
+        data = r'%123478912347^892^^^2357892351^AI, HA L'
+        assert process(data) == 'AI, HA L'
+
+def main():
+        for line in fileinput.input():
+                print process(line)
+
+test()
+main()
---
+++ spreadsheet_functions.py
@@ -0,0 +1,39 @@
+import sheets_api_quickstart as 
+from config import MINNEHACK_ID
+
+service = sheets.get_service()
+EXAMPLE_ID = '1cx8xYnIFvoELmbYathI25RXQL70lVwitfgwNBoAu3g0'
+
+# Set to MINNEHACK_ID for production.
+sheetID = EXAMPLE_ID
+
+NAMES_COL = 'A1:A'
+ATTENDANCE_COL = 'L'
+
+def formatRange(rangeName):
+    return 'sheet1!' + rangeName
+
+def set(cell, value):
+    rangeName = formatRange(cell)
+    body = { 'values': [[ value ]] }
+    result = service.update(
+        spreadsheetId=sheetID, range=rangeName, valueInputOption='RAW',
+        body=body).execute()
+    return result
+
+def get (rangeName):
+    rangeName = formatRange(rangeName)
+    result = service.get(
+        spreadsheetId=sheetID, range=rangeName).execute()
+    values = result.get('values', [])
+    return values
+
+def get_names():
+     names = get(NAMES_COL)
+     # Sheets returns each cell as a list for some reason.
+     unboxed_names = [ name[0] for name in names ]
+     return unboxed_names
+
+def check_attendance(index):
+    cell = ATTENDANCE_COL + str(index)
+    set(cell, True)
