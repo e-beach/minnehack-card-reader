26b0be8ba8c84821c5ca0ac4838d20744b4f8ee2
Elliott Beach <elliott2.71828@gmail.com>
2016-12-16 17:14:02 -0600

    refactored. can now read stateid, can catch exception.

--- input.py
+++ input.py
@@ -2,9 +2,13 @@ import fileinput
 import re
 import spreadsheet_functions
 
-FIRST_AND_LAST_NAME = re.compile(r'[A-Z ,\.\'-]+')
+# '^' is the field separator for the MSR100 .
+FIRST_AND_LAST_NAME = re.compile(r'(?<=\^)[A-Z ,\.\'-]+')
 
 names = [string.lower() for string in spreadsheet_functions.get_names()]
+
+class ParseException(Exception):
+    pass
 
 def index_of(list, item):
     try:
@@ -14,6 +18,8 @@ def index_of(list, item):
 
 def parse(line):
     matches = FIRST_AND_LAST_NAME.findall(line)
+    if not matches:
+        raise ParseException
     # Grab the last group. There may be useless matches before the end.
     name = matches[-1]
     # drop final initial, which is for middle name
@@ -23,14 +29,18 @@ def parse(line):
     return formatted_name
 
 def process(line):
-    name = parse(line)
-    index = index_of(names, name)
-    if index == -1:
-        print 'Name %s is not registered.' % name
+    try:
+        name = parse(line)
+    except ParseException:
+        print 'CARD READ ERROR'
     else:
-        spreadsheet_functions.check_attendance(index)
-        print 'Name %s is registered.' % name
-    return name
+        index = index_of(names, name)
+        if index == -1:
+            print 'Name %s is not registered.' % name
+        else:
+            spreadsheet_functions.check_attendance(index)
+            print 'Name %s is registered.' % name
+        return name
 
 def test():
         data = r'%123478912347^892^^^2357892351^BEACH, ELLIOTT T?'
