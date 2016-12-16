7fae4e3d63a14fa45bbf6da2e1e459236bfdee56
Elliott Beach <elliott2.71828@gmail.com>
2016-12-16 17:02:37 -0600

    add parse function

--- input.py
+++ input.py
@@ -12,31 +12,36 @@ def index_of(list, item):
     except ValueError:
         return -1
 
+def parse(line):
+    matches = FIRST_AND_LAST_NAME.findall(line)
+    # Grab the last group. There may be useless matches before the end.
+    name = matches[-1]
+    # drop final initial, which is for middle name
+    first_and_last = name.split()[0] + " " + name.split()[1]
+    # Convert to lower case.
+    formatted_name = first_and_last.lower()
+    return formatted_name
+
 def process(line):
-        matches = FIRST_AND_LAST_NAME.findall(line)
-        # Grab the last group. There may be useless matches before the end.
-        name = matches[-1]
-        # drop final initial, which is for middle name
-        first_and_last = name.split()[0] + " " + name.split()[1]
-        # Convert to lower case.
-        formatted_name = first_and_last.lower()
-        index = index_of(names, formatted_name)
-        if index == -1:
-            print 'Name %s is not registered.' % name
-        else:
-            spreadsheet_functions.check_attendance(index)
-            print 'Name %s is registered.' % name
-        return name
+    name = parse(line)
+    index = index_of(names, name)
+    if index == -1:
+        print 'Name %s is not registered.' % name
+    else:
+        spreadsheet_functions.check_attendance(index)
+        print 'Name %s is registered.' % name
+    return name
 
 def test():
         data = r'%123478912347^892^^^2357892351^BEACH, ELLIOTT T?'
-        assert process(data) == 'BEACH, ELLIOTT T'
+        assert process(data) == 'beach, elliott'
         data = r'%123478912347^892^^^2357892351^AI, HA L'
-        assert process(data) == 'AI, HA L'
+        assert process(data) == 'ai, ha'
 
 def main():
-        for line in fileinput.input():
-                print process(line)
+        while True:
+            print ">>>"
+            process( raw_input() )
 
 test()
 main()
